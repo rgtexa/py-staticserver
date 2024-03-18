33221bc1da2879d5bf0bf8fa27cd1ebb344cf207
rgtexa <dcscott@gmail.com>
2024-03-18 09:39:39 -0400

    add text node to html node

--- src/main.py
+++ src/main.py
@@ -1,9 +1,33 @@
-from textnode import TextNode
-
+from textnode import TextNode, text_node_to_html_node
 
 def main():
-    tn = TextNode("Hello, World!", "text", "https://www.boot.dev")
-    print(tn)
+    tn1 = TextNode("This is a bold node", "bold")
+    tn2 = TextNode("This is an italic node", "italic")
+    tn3 = TextNode("This is a code node", "code")
+    tn4 = TextNode("This is a link node", "link", "https://www.boot.dev")
+    tn5 = TextNode("This is an image node", "image", "test.png")
+    tn6 = TextNode("This is a text node", "text")
+
+    print(text_node_to_html_node(tn1))
+    print(text_node_to_html_node(tn2))
+    print(text_node_to_html_node(tn3))
+    print(text_node_to_html_node(tn4))
+    print(text_node_to_html_node(tn5))
+    print(text_node_to_html_node(tn6))
+
+    hn1 = text_node_to_html_node(tn1)
+    hn2 = text_node_to_html_node(tn2)
+    hn3 = text_node_to_html_node(tn3)
+    hn4 = text_node_to_html_node(tn4)
+    hn5 = text_node_to_html_node(tn5)
+    hn6 = text_node_to_html_node(tn6)
+
+    print(hn1.to_html())
+    print(hn2.to_html())
+    print(hn3.to_html())
+    print(hn4.to_html())
+    print(hn5.to_html())
+    print(hn6.to_html())
     
     
 if __name__ == "__main__":
--- src/textnode.py
+++ src/textnode.py
@@ -1,3 +1,4 @@
+import leafnode
 class TextNode():
     def __init__(self, text, text_type, url = None):
         self.text = text
@@ -9,3 +10,20 @@ class TextNode():
 
     def __repr__(self) -> str:
         return f"TextNode({self.text}, {self.text_type}, {self.url})"
+
+def text_node_to_html_node(text_node):
+    match text_node.text_type:
+        case "bold":
+            return leafnode.LeafNode("b", text_node.text)
+        case "italic":
+            return leafnode.LeafNode("i", text_node.text)
+        case "code":
+            return leafnode.LeafNode("code", text_node.text)
+        case "link":
+            return leafnode.LeafNode("a", text_node.text, {"href": text_node.url})
+        case "image":
+            return leafnode.LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
+        case "text":
+            return leafnode.LeafNode(None, text_node.text)
+        case _:
+            raise ValueError(f"Unknown text type: {text_node.text_type}")
